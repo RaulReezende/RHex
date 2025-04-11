1907fb30e774ba7630d4f5e3391526740e30f1b9
Raul Rezende <raul31fish@gmail.com>
2025-04-11 11:17:38 -0300

    updating migration with validations

--- RHex.Infrastructure/Migrations/20250411141342_AllValidations.Designer.cs
+++ RHex.Infrastructure/Migrations/20250411141342_AllValidations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RHex.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using RHex.Infrastructure.Data;
 namespace RHex.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250411141342_AllValidations")]
+    partial class AllValidations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
